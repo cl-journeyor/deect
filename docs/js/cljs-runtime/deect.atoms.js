c94e476824f8e5a6a521d13cfa28b6989b6effbe
cl-journeyor <genericobject711@yahoo.com>
2025-11-28 11:49:26 -0800

    Removed /docs/js

--- docs/js/cljs-runtime/deect.atoms.js
+++
@@ -1,5 +0,0 @@
-goog.provide('deect.atoms');
-deect.atoms.selected_lang_item = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(null);
-deect.atoms.selected_code = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"lang","lang",-1819677104),null,new cljs.core.Keyword(null,"page","page",849072397),(0)], null),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"lang","lang",-1819677104),null,new cljs.core.Keyword(null,"page","page",849072397),(0)], null)], null));
-
-//# sourceMappingURL=deect.atoms.js.map
